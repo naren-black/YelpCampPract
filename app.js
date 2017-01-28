ad1ac624e62aca74a81e5ed08f7540edefb6551d
Naren Nath <narenblue@gmail.com>
2017-01-28 15:02:43 +0000

    Add styles madi

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var express = require("express"),
   
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 seedDB();
 
