5e4a97cfe30571b32c165578f7900de2b40c226a
Naren Nath <narenblue@gmail.com>
2017-03-04 01:42:58 +0000

    Create delete and update

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -58,6 +58,27 @@ router.get("/:id/edit", function(req, res) {
   });
 });
 
+router.put("/:id", function(req, res) {
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
+    if(err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });  
+});
+
+router.delete("/:id", function(req, res) {
+  Campground.findByIdAndRemove(req.params.id, function(err) {
+    if(err) {
+      res.redirect("/campgrounds");
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+  
+});
+
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()) {
     return next();
